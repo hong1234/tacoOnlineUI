6acba0f87489cfbfa1b9718afcb374d5c19ee7e8
hong le <vuanhde@yahoo.de>
2026-02-20 13:40:46 +0100

    5c.commit

--- src/Order.jsx
+++ src/Order.jsx
@@ -23,42 +23,44 @@ const Order = () => {
       {/* <p className="card-text">{product.unitPrice} $</p> */}
       {/* <button onClick={handleAddItem}>add to cart</button> */}
       {/* <button onClick={() => mutate(productId)}>add to cart</button> */}
-      <div className="">
-        {/* <h3>Order-Id: {order.id}</h3> */}
-        <h1 className="font-heading text-2xl font-bold">Thank for your Order</h1>
-        ordered at: {order.placedAt} <br />
-        OrderID: <strong>{order.uuid}</strong>
-        {/* OrderID: <strong>{order.uuid.substring(order.uuid.length - 12)}</strong> */}
-        {/* <br />
-        {order.uuid} */}
-        <br />
-        <br />
-        <p className="font-bold">Kunde: {order.deliveryName}</p>
-        Adresse: {order.deliveryStreet} - {order.deliveryCity} <br />
-        Tel-Nummer: {order.ccNumber} <br />
-        <br />
-        <p className="font-bold">Total-Price: {order.totalPrice}</p>
-        Items-Price: {order.itemsPrice} <br />
-        Shipment-Price: {order.shipmentPrice} <br />
-        <br />
-        {order.items.map((item) => (
-          <div key={item.id}>
-            BanhMy-ID: {item.id} <br />
-            Quantity: {item.qty} <br />
-            UnitPrice: {item.unitPrice} $<br />
-            SumPrice: {item.sumPrice} $<br />
-            Ingredients:
-            {item.ingredients
-              .sort((a, b) => a.id - b.id)
-              .map((ing) => (
-                <div key={ing.id}>
-                  {ing.name} - {ing.ingredientType} <br />
-                </div>
-              ))}
-            <br />
-          </div>
-        ))}
-      </div>
+
+      {order.id > 0 ? (
+        <div className="">
+          <h1 className="font-heading text-2xl font-bold">Thank for your Order</h1>
+          ordered at: {order.placedAt} <br />
+          OrderID: <strong>{order.uuid}</strong>
+          {/* OrderID: <strong>{order.uuid.substring(order.uuid.length - 12)}</strong> */}
+          <br />
+          <br />
+          <p className="font-bold">Kunde: {order.deliveryName}</p>
+          Adresse: {order.deliveryStreet} - {order.deliveryCity} <br />
+          Tel-Nummer: {order.ccNumber} <br />
+          <br />
+          <p className="font-bold">Total-Price: {order.totalPrice}</p>
+          Items-Price: {order.itemsPrice} <br />
+          Shipment-Price: {order.shipmentPrice} <br />
+          <br />
+          {order.items.map((item) => (
+            <div key={item.id}>
+              BanhMy-ID: {item.id} <br />
+              Quantity: {item.qty} <br />
+              UnitPrice: {item.unitPrice} $<br />
+              SumPrice: {item.sumPrice} $<br />
+              Ingredients:
+              {item.ingredients
+                .sort((a, b) => a.id - b.id)
+                .map((ing) => (
+                  <div key={ing.id}>
+                    {ing.name} - {ing.ingredientType} <br />
+                  </div>
+                ))}
+              <br />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h2>THE ORDER ( ID={order.uuid} ) IS NOT FOUND</h2>
+      )}
     </div>
   );
 };
